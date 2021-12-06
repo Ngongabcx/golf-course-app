3fcc501b9a1599ed684126560e44a3e9b1cbd5dc
Ng'onga Bwalya <bwalyangonga@gmail.com>
2021-12-06 13:35:35 +0200

    Getting all competitions from database

---
+++ lib/app/modules/ActiveGameScreen/providers/active_games_provider.dart
@@ -0,0 +1,22 @@
+import 'package:gcms/app/modules/SetupScreen/competition_model.dart';
+import 'package:gcms/constants/constant.dart';
+import 'package:get/get.dart';
+
+class ActiveGamesProvider extends GetConnect {
+  Future<Competition> getActiveMatches() async {
+    try {
+      final response = await get("$kApiBaseURL/competitions");
+      if (response.status.hasError) {
+        print('<<===GET COMPETITIONS  ERROR==> ${response.statusText}');
+        return Future.error(response.statusText);
+      } else {
+        final resp = competitionFromJson(response.bodyString);
+        print('<<===GET COMPETITIONS  SUCCESSFUL==> ${response.bodyString}');
+        return resp;
+      }
+    } catch (exception) {
+      print('<<===GET COMPETITIONS EXCEPTION2==> $exception');
+      return Future.error(exception);
+    }
+  }
+}
